a503fe1c6c9366330c352eec87331b5279c10f9a
Yesmy Darlery Escobar Escobar <yescobar2@miumg.edu.gt>
2026-04-08 23:40:28 -0600

    Clase Factura

---
+++ src/getAndset/Factura.java
@@ -0,0 +1,28 @@
+package getAndset;
+
+public class Factura {
+    private String codigo;
+    private int cantidad;
+    private double precioUnitario;
+
+    public Factura(String codigo, int cantidad, double precio) {
+        this.codigo = codigo;
+        setCantidad(cantidad);
+        setPrecio(precio);
+    }
+
+    public void setCantidad(int c) {
+        if (c <= 0) throw new IllegalArgumentException("Cantidad debe ser mayor a 0.");
+        this.cantidad = c;
+    }
+
+    public void setPrecio(double p) {
+        if (p <= 0) throw new IllegalArgumentException("Precio debe ser mayor a 0.");
+        this.precioUnitario = p;
+    }
+
+    public void mostrar() {
+        double total = cantidad * precioUnitario;
+        System.out.println("Factura: " + codigo + "  Total: " + total);
+    }
+}
