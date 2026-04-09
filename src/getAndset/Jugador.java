f5d30b0acef8def415981c7595ca13d8ab3bea74
Yesmy Darlery Escobar Escobar <yescobar2@miumg.edu.gt>
2026-04-08 23:16:36 -0600

    Clase Jugador

--- src/getAndset/Jugador.java
+++ src/getAndset/Jugador.java
@@ -1,5 +1,22 @@
 package getAndset;
 
 public class Jugador {
+    private String nombre;
+    private int puntajeActual, puntajeMaximo;
 
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+        this.puntajeActual = 0;
+        this.puntajeMaximo = 0;
+    }
+
+    public void actualizarPuntaje(int nuevo) {
+        if (nuevo < 0) throw new IllegalArgumentException("El puntaje no puede ser negativo.");
+        this.puntajeActual = nuevo;
+        if (nuevo > puntajeMaximo) this.puntajeMaximo = nuevo;
+    }
+
+    public void mostrar() {
+        System.out.println("Jugador: " + nombre + " | Actual: " + puntajeActual + " | Máximo: " + puntajeMaximo);
+    }
 }
